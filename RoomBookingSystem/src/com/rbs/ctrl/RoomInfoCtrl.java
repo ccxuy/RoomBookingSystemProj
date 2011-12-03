bce539ed8aa0144e5890badbc770cabaaca7d9a9
sophia <cs74lele@gmail.com>
2011-12-04 00:48:10 +0800

    hey

--- RoomBookingSystem/src/com/rbs/ctrl/RoomInfoCtrl.java
+++ RoomBookingSystem/src/com/rbs/ctrl/RoomInfoCtrl.java
@@ -1,44 +1,53 @@
 package com.rbs.ctrl;
 
 import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import com.rbs.model.RoomInfo;
+import com.rbs.model.User;
 
 public class RoomInfoCtrl {
-
+	private SessionFactory sessionFactory;
 	/**
 	 * 
 	 * @param roomNum
-	 * @return 
+	 * @return List<RoomInfo>
 	 */
-	public RoomInfo queryRoom(String roomNum) {
-		throw new UnsupportedOperationException();
+	public List<RoomInfo> queryRoom(String roomNum) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		String hql = "select R from RoomInfo R where R.roomID='"+roomNum+"'";
+		List result = session.createQuery(hql).list();
+		/*
+		for ( RoomInfo u : (List<RoomInfo>) result ) {
+			System.out.println( "Roominfo (" + u.getDateBegin() + ") : " + u.getDateEnd() );
+		}
+		*/
+		session.getTransaction().commit();
+		session.close();
+		return result;
 	}
 
-	/**
-	 * 
-	 * @param roomInfoID
-	 * @param dateBegin
-	 * @param dateEnd
-	 * @param week
-	 * @param timeBegin
-	 * @param timeEnd
-	 * @param capacity
-	 * @param facilities
-	 * @return 
-	 */
-	public int uploadRoomInfo(String roomInfoID, Date dateBegin, Date dateEnd, String week, Date timeBegin, Date timeEnd, int capacity, String facilities) {
-		throw new UnsupportedOperationException();
-	}
+	
 
 	/**
 	 * 
 	 * @param oldRoom
-	 * @param newRoom
 	 * @return 
 	 */
-	public int updateRoomInfo(RoomInfo oldRoom, RoomInfo newRoom) {
-		throw new UnsupportedOperationException();
+	public int maintainRoomInfo(RoomInfo newRoom) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.update(newRoom);
+		
+		session.getTransaction().commit();
+		session.close();
+		return 1;
 	}
 
 	/**
--- RoomBookingSystem/src/com/rbs/ctrl/UserCtrl.java
+++ RoomBookingSystem/src/com/rbs/ctrl/UserCtrl.java
@@ -1,87 +1,159 @@
 package com.rbs.ctrl;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import com.rbs.dbAccess.hbmOperation;
+import org.hibernate.SessionFactory;
+
 import com.rbs.model.User;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
 
+import com.rbs.model.*;
 public class UserCtrl extends RoomInfoCtrl {
-	public final String FAIL = "FAIL";
-	public final String SUCCESS = "SUCCESS";
 	
-
-	public final String R_NOTREG = "not registered";
-	public final String ROLE_USER = "ROLE_USER";
-	public final String ROLE_MGR = "ROLE_MGR";
-	public final String ROLE_ADM = "ROLE_ADM";
+	public final String R_REGISTER = "NOT REGISTERED"; 
+	public final String R_APPLICANT = "APPLICANT"; 
+	public final String R_MANAGER = "MANAGER"; 
+	public final String R_ADMIN = "ADMIN"; 
 	
-	private Session session;
-	
-	public UserCtrl(){
-		session = new hbmOperation().getSession();
-	}
+	private SessionFactory sessionFactory;
 	/**
 	 * 
 	 * @return 
 	 */
 	public void logout() {
-		throw new UnsupportedOperationException();
 		
 	}
 
 	/**
 	 * 
 	 * @param name
-	 * @param password
+	 * @param password  OK
 	 * @return 
 	 */
-	public String login(String name, String password) {
-		User entity = null;
-        Query q = session.createQuery("from User u where u.name = ? and u.password = ?");
-        q.setString(0, name);
-        q.setString(1, password);
-        if(q.list().size() > 0)
-        {
-            return SUCCESS;
-        }
-        return FAIL;
-	}
+	public int login(String name, String password) {
 
+		Session session = sessionFactory.openSession();
+		Transaction tran=session.beginTransaction();
+		String hql = "select u.* from User u where u.name='"+name+"' and u.password='"+password+"'";
+		Query query=session.createSQLQuery(hql);
+		List<User> list=query.list();
+		
+		if(list.size()==1){
+			tran.commit();
+        	session.close();
+			return 1; //successful to login
+		}
+		else{
+			tran.commit();
+        	session.close();
+        	return 0; // fail to login
+		}
+	}
+	public int login(User u){
+		return login(u.getName(),u.getPassword());
+	}
+	
 	/**
 	 * 
-	 * @param newUser
+	 * @param newUser   OK
 	 * @return 
 	 */
 	public int register(User newUser) {
-		throw new UnsupportedOperationException();
+		Session session = sessionFactory.openSession();
+		Transaction tran=session.beginTransaction();
+		String hql = "select u.* from User u where u.name='"+newUser.getName()+"'";
+		Query query=session.createSQLQuery(hql);
+		List<User> list=query.list();
+		if(list.size()!=0){
+			//System.out.print("User is already existed. ");
+			tran.commit();
+	        session.close();
+	        return 0;
+		}
+		else{
+		    session = sessionFactory.openSession();
+			tran=session.beginTransaction();
+	        //System.out.print("Register Successful!");
+			session.save(newUser);
+			tran.commit();
+	        session.close();
+	        return 1;
+		}
 	}
 
 	/**
 	 * 
-	 * @param u
+	 * @param OK
 	 * @return 
 	 */
-	public int changePermission(User u) {
-		throw new UnsupportedOperationException();
+	public int setManager(User u) {
+		
+		
+		
+		//Query query=session.createSQLQuery(queryState);
+	    //List<User> list=query.list();
+		//Iterator it =list.iterator();
+		//while(it.hasNext()){
+		//Object[] result=(Object[])it.next();
+		//Sytem.out.println(result[3]);	
+		
+		Session session = sessionFactory.openSession();
+		Transaction tran=session.beginTransaction();
+		u.setPermission(R_MANAGER);
+		session.save(u);
+		//String hql = "update User as u1 set permission='"+R_MANAGER+"' where u1.name='"+u.getName()+"'";		
+		//Query query=session.createQuery(hql);
+		//int ret=query.executeUpdate();
+		//System.out.println("Upload Records->"+ret);
+		tran.commit();
+        session.close();
+        return 1;
 	}
-
+	
 	/**
 	 * 
-	 * @param u
+	 * @param OK
 	 * @return 
 	 */
-	public int addUser(User u) {
-		throw new UnsupportedOperationException();
+	
+     public int setApplicant(User u) {
+		
+		Session session = sessionFactory.openSession();
+		Transaction tran=session.beginTransaction();
+		//String hql = "update User as u1 set permission='0' where u1.name='"+u.getName()+"'";		
+		//Query query=session.createQuery(hql);
+		//int ret=query.executeUpdate();
+		//System.out.println("Upload Records->"+ret);
+		u.setPermission(R_APPLICANT);
+		session.save(u);
+		tran.commit();
+        session.close();
+        return 1;
 	}
+
+	
 
 	/**
 	 * 
-	 * @param u
+	 * @param u  OK
 	 * @return 
 	 */
 	public int deleteUser(User u) {
-		throw new UnsupportedOperationException();
+		Session session = sessionFactory.openSession();
+		Transaction tran=session.beginTransaction();
+		String hql = "delete from User u1 where u1.name='"+u.getName()+"'";
+		int number=session.createQuery(hql).executeUpdate();
+		//session.delete(u);
+		// number=1, successful 0,fail
+		tran.commit();
+		session.close();
+		return number;		
 	}
 
 }
